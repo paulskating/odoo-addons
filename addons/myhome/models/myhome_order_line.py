a58e9a58b0126651df7275111f07d077cbd3df9f
minjie <a149889685@126.com>
2016-05-24 17:40:35 +0800

    20160524-end

--- addons/myhome/models/myhome_order_line.py
+++ addons/myhome/models/myhome_order_line.py
@@ -6,4 +6,15 @@ class MyhomeOrderLine(models.Model):
     _name = "myhome.order_line"
     order_id = fields.Many2one("myhome.order")
     bank_id = fields.Many2one("myhome.bank",string="Bank_Name",required=True)
+    begin = fields.Float()
+    end = fields.Float()
+    account = fields.Float(compute="_compute_account")
+    @api.multi
+    def _compute_account(self):
+        for line in self:
+            line.account = line.end - line.begin
+
+
+
+
 
