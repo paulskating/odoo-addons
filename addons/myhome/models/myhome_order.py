38312042cba2aade7529f87c8437fee329805971
minjie <a149889685@126.com>
2016-05-27 17:52:16 +0800

    20160527-end

--- addons/myhome/models/myhome_order.py
+++ addons/myhome/models/myhome_order.py
@@ -9,8 +9,16 @@ class MyhomeOrder(models.Model):
     state = fields.Char()
     order_line = fields.One2many("myhome.order_line", "order_id")
     notes = fields.Text()
+    untaxed = fields.Float(string='Untaxed Amount', store=True, readonly=True, compute='_amount_all', track_visibility='always')
+    tax = fields.Float(string='Taxes', store=True, readonly=True, compute='amount_all')
+    total = fields.Float(string='Total', store=True, readonly=True, compute='amount_all')
+
 
     @api.multi
     def action_confirm(self):
+        pass
+
+    @api.multi
+    def amount_all(self):
         pass
 
