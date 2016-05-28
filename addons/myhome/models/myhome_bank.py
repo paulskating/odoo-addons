aeda16a8333a77f756ab908cadda8eff3af069da
minjie <minjie@jiudaotech.com>
2016-05-29 01:22:40 +0800

    home-end

--- addons/myhome/models/myhome_bank.py
+++ addons/myhome/models/myhome_bank.py
@@ -8,7 +8,6 @@ class MyhomeBank(models.Model):
 
     bank_name = fields.Char()
     bank_category = fields.Char()
-    bank_a = fields.Many2many()
 
 
     line = fields.One2many("myhome.order_line","bank_id")
